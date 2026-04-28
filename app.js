a29693f87a96eae961fa5d40ddcd3aa2fb1f5179
rajeshark <101121907+rajeshark@users.noreply.github.com>
2026-04-28 11:36:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Microservice rajesha rk');
+  res.send('Hello from Microservice cloudifyops');
 });
 
 const PORT = 3000;
